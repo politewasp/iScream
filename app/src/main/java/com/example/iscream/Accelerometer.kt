109a9897e75ccd26b508ae72f400653fae7834c6
politewasp <mi.matson42@gmail.com>
2021-05-08 15:12:53 -0500

    commenting and cleaning

--- app/src/main/java/com/example/iscream/Accelerometer.kt
+++ app/src/main/java/com/example/iscream/Accelerometer.kt
@@ -8,24 +8,26 @@ import android.hardware.SensorEventListener
 import kotlin.math.sqrt
 
 class Accelerometer : SensorEventListener {
+    // never used
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        println("accuracy changed")
+        // pass
     }
 
+    // runs when sensor values change IF scream button enabled
     override fun onSensorChanged(event: SensorEvent?) {
+        // reads force acting upon x, y, and z axis
+        // ex: zval will return 9.8 when sitting still
         val xval : Float = event?.values?.get(0) ?: 0F
         val yval : Float = event?.values?.get(1) ?: 0F
         val zval : Float = event?.values?.get(2) ?: 0F
-        //println(timestamp)
 
+        // calculates total force acting on sensor
         val at = sqrt(xval*xval + yval*yval + zval*zval)
 
+        // if total force is close to zero, phone is in free fall
         if(at < 1 && at > -1){
             println("IM FALLING")
         }
-
-
-
     }
 
 }
--- app/src/main/java/com/example/iscream/MainActivity.kt
+++ app/src/main/java/com/example/iscream/MainActivity.kt
@@ -10,36 +10,45 @@ import android.widget.ToggleButton
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // set screen to res/layout/activity_main.xml
         setContentView(R.layout.activity_main)
 
+        // create sensor of the type accelerometer and instantiate a listener for the sensor
         val sensorManager : SensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
         val sensor: Sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
         val accelerometer = Accelerometer()
 
-
+        // check if toggleButton is pressed or not
         val toggle: ToggleButton = findViewById(R.id.toggleButton)
         toggle.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // provide haptic feedback upon button press
                 toggle.performHapticFeedback(
                     HapticFeedbackConstants.VIRTUAL_KEY,
                     HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
                 )
                 println("BEGIN DATA")
+
+                // enable sensor data reading
                 sensorManager.registerListener(accelerometer, sensor, SensorManager.SENSOR_DELAY_NORMAL)
             }
             else {
+                // provide haptic feedback upon button press
                 toggle.performHapticFeedback(
                     HapticFeedbackConstants.VIRTUAL_KEY,
                     HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING
                 )
                 println("END DATA")
+
+                // disable sensor data reading
                 sensorManager.unregisterListener(accelerometer)
             }
         }
 
     }
 
+    // ensures that if other activities are launched, no animation plays upon switching
     override fun onPause() {
         super.onPause()
         overridePendingTransition(0, 0)
